a1aaca186d5980eec817a703e6576efb21d757cb
Ubuntu <ubuntu@ip-172-31-11-193.ec2.internal>
2023-10-27 11:13:18 +0000

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# aws-eks-cicd-pipeline-
-This is public repo and i will create cicd pipeline in aws eks
--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
